cbf96893b5d08460468f313e68f5b33ef18dd1d6
chibicoffeelover <yuniorf02@protonmail.com>
2026-05-31 00:13:50 +0900

    build(kotlin): apply android.builtInKotlin=false + restore kotlin.android (KSP1 path, B-08-03/D-BIK)
    - Add android.builtInKotlin=false to gradle.properties (D-BIK deferral)
    - Re-add org.jetbrains.kotlin.android at the 5 sites dropped in fa25942:
      application + library convention plugins, root alias, baselineprofile, catalog
    - Keep bare-CommonExtension fix + KotlinAndroidProjectExtension block (AGP-9 API facts)

--- build-logic/convention/src/main/kotlin/io/stashapp/android/buildlogic/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/io/stashapp/android/buildlogic/AndroidLibraryConventionPlugin.kt
@@ -11,6 +11,7 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
         with(target) {
             with(pluginManager) {
                 apply("com.android.library")
+                apply("org.jetbrains.kotlin.android")
             }
             extensions.configure<LibraryExtension> {
                 configureKotlinAndroid(this)
